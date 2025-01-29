a63dc76a1428cac1c7a3619e4aebb9a6fb9e00ce
seo7905 <wjddks64@gmail.com>
2025-01-29 22:25:29 +0900

    2025-01-29 "극장페이지 작업중"

--- src/main/java/action/user/theater/TheaterMovieAction.java
+++ src/main/java/action/user/theater/TheaterMovieAction.java
@@ -3,6 +3,8 @@ package action.user.theater;
 import action.Action;
 import mybatis.dao.TheaterDAO;
 import mybatis.vo.TheaterMovieVO;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,60 +13,43 @@ import java.io.IOException;
 public class TheaterMovieAction implements Action {
     @Override
     public String execute(HttpServletRequest request, HttpServletResponse response) throws IOException {
-
         String theaterIdx = request.getParameter("theaterIdx");
         String targetDate = request.getParameter("targetDate"); // targetDate로 수정
-        System.out.println("theaterIdx:"+theaterIdx);
-        System.out.println("targetDate:"+targetDate);
 
         // 극장 영화 정보 가져오기
         TheaterMovieVO[] movieList = TheaterDAO.getMovie(theaterIdx, targetDate);
-
 
         // 응답 유형 설정
         response.setContentType("application/json");
         response.setCharacterEncoding("UTF-8");
 
-        if (movieList.length > 0) {
-            // JSON 형식으로 영화 정보를 반환
-            StringBuilder jsonResponse = new StringBuilder();
-            jsonResponse.append("[");
+        JSONArray jsonArray = new JSONArray();
 
-            for (int i = 0; i < movieList.length; i++) {
-                TheaterMovieVO movie = movieList[i];
-                jsonResponse.append("{");
-                jsonResponse.append("\"movieTitle\": \"").append(movie.getMovieTitle()).append("\", ");
-                jsonResponse.append("\"screenName\": \"").append(movie.getScreenName()).append("\", ");
-                jsonResponse.append("\"remainSeat\": \"").append(movie.getRemainSeat()).append("\", ");
-                jsonResponse.append("\"remainSeat\": \"").append(movie.getRemainSeat()).append("\", ");
-                jsonResponse.append("\"screenSeatCount\": \"").append(movie.getScreenSeatCount()).append("\", ");
-                jsonResponse.append("\"movieTime\": \"").append(movie.getMovieTime()).append("\", ");
-                jsonResponse.append("\"movieGrade\": \"").append(movie.getMovieGrade()).append("\", ");
-                jsonResponse.append("\"screenType\": \"").append(movie.getScreenType()).append("\", ");
-                jsonResponse.append("\"startTime\": \"").append(movie.getStartTime()).append("\", ");
-                jsonResponse.append("\"screenIdx\": \"").append(movie.getScreenIdx()).append("\", ");
-                jsonResponse.append("\"movieIdx\": \"").append(movie.getMovieIdx()).append("\", ");
-                jsonResponse.append("\"timetableIdx\": \"").append(movie.getTimeTableIdx()).append("\", ");
-                jsonResponse.append("\"timetableStartTime\": \"").append(movie.getTimetableStartTime()).append("\"");
+        for (TheaterMovieVO movie : movieList) {
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("movieTitle", movie.getMovieTitle());
+            jsonObject.put("screenName", movie.getScreenName());
+            jsonObject.put("remainSeat", movie.getRemainSeat());
+            jsonObject.put("screenSeatCount", movie.getScreenSeatCount());
+            jsonObject.put("movieTime", movie.getMovieTime());
+            jsonObject.put("movieGrade", movie.getMovieGrade());
+            jsonObject.put("screenType", movie.getScreenType());
+            jsonObject.put("startTime", movie.getStartTime());
+            jsonObject.put("screenIdx", movie.getScreenIdx());
+            jsonObject.put("movieIdx", movie.getMovieIdx());
+            jsonObject.put("timetableIdx", movie.getTimeTableIdx());
+            jsonObject.put("timetableStartTime", movie.getTimetableStartTime());
 
+            jsonArray.put(jsonObject);
+        }
 
-                jsonResponse.append("}");
-
-                if (i < movieList.length - 1) {
-                    jsonResponse.append(", ");
-                }
-            }
-
-            jsonResponse.append("]");
-
-            response.getWriter().write(jsonResponse.toString());
-        } else {
-            // 영화가 없으면 메시지 반환
+        if (jsonArray.isEmpty()) {
             response.getWriter().write("{\"message\": \"현재 상영 중인 영화가 없습니다.\"}");
+        } else {
+            response.getWriter().write(jsonArray.toString());
         }
 
         response.getWriter().flush();
-        response.getWriter().close();
         return null;
     }
 }
