a98f893eb36afc985255fa8282f7ebf5568b2b54
mmi-ing <tpgns252@gmail.com>
2025-01-30 18:26:44 +0900

    2025-01-30 쿠폰 페이지 작업 완료 (드롭박스 기능 제외)

--- src/main/java/mybatis/dao/CouponDAO.java
+++ src/main/java/mybatis/dao/CouponDAO.java
@@ -8,22 +8,58 @@ import java.util.HashMap;
 import java.util.List;
 
 public class CouponDAO {
-    // 사용자 쿠폰 조회
-    public static CouponVO[] getList(String userIdx, int begin, int end) {
+
+    public static int getTotalCount(String userIdx) {
+        int totalCount = 0;
+        SqlSession ss = FactoryService.getFactory().openSession();
+        totalCount = ss.selectOne("coupon.totalCount", userIdx);
+        ss.close();
+        return totalCount;
+    }
+
+    // 사용자 쿠폰 조회 페이징
+    public static CouponVO[] getList(String userIdx, int begin, int numPerPage) {
         CouponVO[] ar = null;
 
         HashMap<String, Object> map = new HashMap<String, Object>();
         map.put("userIdx", userIdx); // 사용자 ID
-        map.put("begin", begin);     // 시작 인덱스
-        map.put("end", end);         // 끝 인덱스
+        map.put("begin", begin);
+        map.put("numPerPage", numPerPage);
+
 
         SqlSession ss = FactoryService.getFactory().openSession();
         List<CouponVO> list = ss.selectList("coupon.userCoupon", map);
+
         if(list != null && !list.isEmpty()) {
             ar = new CouponVO[list.size()];
             list.toArray(ar);
         }
+
         ss.close();
+        return ar;
+    }
+
+    // 필터링만 적용 (페이징 없음, AJAX 요청용)
+    public static CouponVO[] getListNoPaging(String userIdx, String filter) {
+        CouponVO[] ar = null;
+
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("userIdx", userIdx);
+        map.put("filter", filter);
+
+        SqlSession ss = FactoryService.getFactory().openSession();
+        try {
+            List<CouponVO> list = ss.selectList("coupon.userCouponNoPaging", map);
+
+            if (list != null && !list.isEmpty()) {
+                ar = new CouponVO[list.size()];
+                list.toArray(ar);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            ss.close();
+        }
         return ar;
     }
 }
