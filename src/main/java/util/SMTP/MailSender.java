280e52523f1e79f3da98acf8c3926e72de0ff0a2
dpdlcl <dpdlcl0111@gmail.com>
2025-02-24 16:31:51 +0900

    tmp

--- src/main/java/util/SMTP/MailSender.java
+++ src/main/java/util/SMTP/MailSender.java
@@ -6,9 +6,15 @@ import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+
+import util.ConfigReader;
+
 import java.util.Properties;
 
 public class MailSender {
+
+    private static final String SECRET_EmailUsername = ConfigReader.getEmailUsername();
+
     public static void sendEmail(String recipient, String subject, String content) throws MessagingException {
         Properties props = new Properties();
         props.put("mail.smtp.host", "smtp.gmail.com");
@@ -20,7 +26,7 @@ public class MailSender {
         Session session = Session.getInstance(props, new SMTPAuth());
 
         MimeMessage message = new MimeMessage(session);
-        message.setFrom(new InternetAddress("byul4167@gmail.com"));
+        message.setFrom(new InternetAddress(SECRET_EmailUsername));
         message.addRecipient(Message.RecipientType.TO, new InternetAddress(recipient));
         message.setSubject(subject);
         message.setContent(content, "text/html;charset=utf-8");
